bbb63bd8222f4060856af20786754d29012c4ad1
Akeem Cherry <akeemcherry@zipcodersMBP67.localdomain>
2018-10-26 14:05:37 -0400

    Program to pass the FizzBuzz Test using TDD

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,19 @@
 public class FizzBuzz {
+    public String fizzBuzz(int i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (i % 5 == 0){
+                return "Buzz";
+            }
+
+        else {
+            return Integer.toString(i);
+        }
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzzer = new FizzBuzz();
+
+    @Test
+    public void TestFizzBuzzThree() {
+        String actual = fizzBuzzer.fizzBuzz(3);
+        String expected = "Fizz";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestFizzBuzzFive() {
+        String actual = fizzBuzzer.fizzBuzz(5);
+        String expected = "Buzz";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestFizzBuzzFifteen() {
+        String actual = fizzBuzzer.fizzBuzz(15);
+        String expected = "FizzBuzz";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestFizzBuzzNineteen() {
+        String actual = fizzBuzzer.fizzBuzz(19);
+        String expected = "19";
+        Assert.assertEquals(expected, actual);
+    }
 }
